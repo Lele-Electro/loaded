c6a39862162d9d53425930574f7f767342c37c75
LAntonioRibeiroD <toni101ribeiro@gmail.com>
2024-04-20 22:52:11 +0200

    first progressive web app push for android and ios devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserAuthenticationService } from './services/user authentication/user-authentication.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,38 @@ import { UserAuthenticationService } from './services/user authentication/user-a
 export class AppComponent implements OnInit{
   title = 'loaded';
 
-  constructor(private authService: UserAuthenticationService){}
+  constructor(
+    private authService: UserAuthenticationService,
+    private snackBar: MatSnackBar
+
+  ){}
   ngOnInit(): void {
     this.authService.autoAuthUser();
+
+    if(window.matchMedia('(display-mode:browser').matches) {
+      // we are in the browser
+      if('standaline' in navigator){
+      // only available in Safari
+      this.snackBar.open('Install the App? :)', '',{duration: 3000})
+
+      } else {
+        // We are not on Safari
+        window.addEventListener('beforeinstallprompt', event => {
+          event.preventDefault();
+
+        const sb =  this.snackBar.open('Install the App? :)', '',{duration: 3000});
+          sb.onAction().subscribe( () => {
+          (event as any).prompt();
+          (event as any).userChoice.then ( (result:any) => {
+            if(result.outcome == 'dismissed'){
+              ///DO THIS
+            } else{
+              //Do THAT
+            }
+          })
+          })
+        })
+      }
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +11,7 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { SnackbarMessageComponent } from './components/snackbar-message/snackbar-message.component';
 import { MatButtonModule } from '@angular/material/button';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,13 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
     BrowserAnimationsModule,
     MatInputModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
 
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
